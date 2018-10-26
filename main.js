186d763b1dcfb9a363f46be83e4c87b1f8385132
troinich <43569578+troinich@users.noreply.github.com>
2018-10-26 19:59:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,80 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var quizz;
-    var score = 0;
-    var x = 0;
 
-    var xhr = new XMLHttpRequest();
+    document.addEventListener('DOMContentLoaded', function () {
+        var quizz;
+        var score = 0;
+        var x = 0;
 
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            console.log(xhr.status);
-            quizz = JSON.parse(xhr.responseText);
-            console.log(quizz);
-            display(x);
+        function display(x) {
+            if (x >= 0 && x < quizz.results.length) {
+                document.getElementById('question1').style.display = 'block';
+                document.getElementById('trues').style.display = 'block';
+                document.getElementById('falses').style.display = 'block';
+
+                document.getElementById("question1").innerHTML = quizz.results[x].question;
+                document.getElementById("trues").innerHTML = quizz.results[x].correct_answer;
+                document.getElementById("falses").innerHTML = quizz.results[x].incorrect_answers[0];
+
+            }//end if statement
+
+            else {
+                document.body.innerHTML = "THE END. YOUR SCORE is " + score + " of " + quizz.results.length;
+                document.body.setAttribute("style", "width: 100%; margin:0 auto; font-weight:bold; font-size: 100px; color: white; background-image: url('https://res.cloudinary.com/simpleview/image/upload/crm/poconos/Holiday-Independence-Day-Fireworks-Lake-Wallenpaupack-2-PoconoMtns01_e6c78873-9f82-6be5-8281f3b4afedd280.jpg');background-repeat: no-repeat;     background-size:cover;");
+            }
+
+        };
+
+        function selectedOrDefault(val, dflt) {
+            if (val == "") {
+                return dflt
+            } else {
+                return val
+            }
         }
-    }
 
-    xhr.open('GET', 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean', true);
-    xhr.responseType = 'text';
-    xhr.send();
+        function setComplex() {
+            var difficulty = selectedOrDefault(
+                document.getElementById("difficulty").value, 'easy')
 
-    function display(x) {
-        if (x >= 0 && x < 10) {
-            document.getElementById("question1").innerHTML = quizz.results[x].question;
-            document.getElementById("trues").innerHTML = quizz.results[x].correct_answer;
-            document.getElementById("falses").innerHTML = quizz.results[x].incorrect_answers[0];
-        }//end if statement
+            var quantity = selectedOrDefault(
+                document.getElementById("quantity").value, 5)
 
-        else {
-            document.body.innerHTML = "THE END. YOUR SCORE is " + score + " of " + quizz.results.length;
-            document.body.setAttribute("style", "width: 100%; margin:0 auto; font-weight:bold; font-size: 100px; color: white; background-image: url('https://res.cloudinary.com/simpleview/image/upload/crm/poconos/Holiday-Independence-Day-Fireworks-Lake-Wallenpaupack-2-PoconoMtns01_e6c78873-9f82-6be5-8281f3b4afedd280.jpg');background-repeat: no-repeat;     background-size:cover;");
+            var subject = selectedOrDefault(
+                document.getElementById("subject").value, 9)
+
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    console.log(xhr.status);
+                    quizz = JSON.parse(xhr.responseText);
+                    console.log(quizz);
+                    console.log(quizz.results);
+                    x=0;
+                    display(x);
+                }
+            }
+
+            var difPath = "https://opentdb.com/api.php?amount=" + quantity + "&category=" + subject + "&difficulty=" + difficulty + "&type=boolean";
+            xhr.open('GET', difPath, true);
+            xhr.responseType = 'text';
+            xhr.send();
+
+            //display(x)
         }
-    };//end of function display
 
-    document.getElementById("falses").addEventListener("click", () => {
-        alert("NO, sorry");
-        display(++x);
-    });
+        document.getElementById("go").addEventListener("click", () => {
+            setComplex();
+        });
 
-    document.getElementById("trues").addEventListener("click", () => {
-        alert("Correct! + 1 score");
-        ++score;
-        display(++x);
-        console.log(score);
-    });
+        document.getElementById("falses").addEventListener("click", () => {
+            alert("NO, sorry");
+            display(++x);
+        });
 
-});// end of DOM function
+        document.getElementById("trues").addEventListener("click", () => {
+            alert("Correct! + 1 score")
+            ++score;
+            display(++x);
+            console.log(score);
+        });
+
+    });// end of DOM function
