b01a7aaad4cc5a98a3de0c2bf63bc4df74a5860c
NATALIA <troinicz@gmail.com>
2018-10-28 22:32:22 +0100

    finished input validation

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var quizz;
     var score = 0;
     var x = 0;
+    const allowedDiff = ["easy", "medium", "hard"];
+    var allowedSubjs = { "books": 10, "films": 11, "music": 12 };
 
     goButton = document.getElementById('go')
     var allErrorMess = document.getElementsByClassName("errorMess");
@@ -10,41 +12,57 @@ document.addEventListener('DOMContentLoaded', function () {
     function showErrorMessage(caller, inputIndex) {
         caller.classList.add("error");
         allErrorMess[inputIndex].style.display = "block";
-
         caller.focus();
     }
 
-    function removeErrorMessage(caller, inputIndex) {
+    function hideErrorMessage(caller, inputIndex) {
         caller.classList.remove("error");
         elem = document.getElementsByClassName("errorMess")[inputIndex];
         elem.style.display = "none";
     }
 
-    difficulty.oninput = function () {
-        if (!isNaN(this.value)) {
-            showErrorMessage(this, 0);
+
+    difficulty.onblur = function () {
+        if (isValidDifficulty(this.value)) {
+            hideErrorMessage(this, 0);
         } else {
-            removeErrorMessage(this, 0);
+            showErrorMessage(this, 0);
         }
         setGo();
     };
 
     quantity.oninput = function () {
-        if (isNaN(this.value)) { // input is not a number
-            showErrorMessage(this, 1);
+        if (isValidNumberOfQuestions(this.value)) {
+            hideErrorMessage(this, 1);
         } else {
-            removeErrorMessage(this, 1);
+            showErrorMessage(this, 1);
         }
     };
 
     subject.oninput = function () {
-        if (!isNaN(this.value)) { // if input is a number
-            showErrorMessage(this, 2);
+        if (isValidSubject(this.value)) {
+            hideErrorMessage(this, 2);
         } else {
-            removeErrorMessage(this, 2);
+            showErrorMessage(this, 2);
         }
         setGo();
     };
+
+    function isValidDifficulty(value) {
+        value = value.toLowerCase().trim();
+        return allowedDiff.includes(value);
+    }
+
+    function isValidNumberOfQuestions(value) {
+        value = Math.floor(value)
+        return (value > 0 && value <= 5)
+    }
+
+    function isValidSubject(value) {
+        value = value.toLowerCase().trim();
+        return allowedSubjs.hasOwnProperty(value);
+    }
+
 
     function setGo() {
         if ((difficulty.className !== "error")
@@ -57,7 +75,6 @@ document.addEventListener('DOMContentLoaded', function () {
             goButton.disabled = true;
         }
     }
-
 
 
     function display(x) {
@@ -75,35 +92,24 @@ document.addEventListener('DOMContentLoaded', function () {
         else {
             document.body.innerHTML = "THE END. YOUR SCORE is " + score + " of " + quizz.results.length;
             document.body.setAttribute("style", "width: 100%; margin:0 auto; font-weight:bold; font-size: 100px; color: white; background-imquantity: url('https://res.cloudinary.com/simpleview/imquantity/upload/crm/poconos/Holiday-Independence-Day-Fireworks-Lake-Wallenpaupack-2-PoconoMtns01_e6c78873-9f82-6be5-8281f3b4afedd280.jpg');background-repeat: no-repeat; background-size:cover;");
+            // setInterval(function(){ location.reload(); }, 200); 
         }
-
     };
 
-    allowedDiff = ["easy", "medium", "hard"];
-    var allErrors = document.getElementsByClassName("error");
-    var difError = allErrors[0];
+    function beautify(value) {
+        return value.toLowerCase().trim();
+    }
 
-    allInputs = document.querySelectorAll('input');
-
-    function selectedOrDefault(val, dflt, allowedItems) {
-        val = val.toLowerCase().trim();
-        if (!allowedItems.includes(val)) {
-            return dflt
-        }
-        else {
-            return val;
-        }
+    function getElemValue(id) {
+        value = document.getElementById(id).value;
+        return beautify(value);
     }
 
     function setComplex() {
-        var difficulty = selectedOrDefault(
-            document.getElementById("difficulty").value, 'easy', allowedDiff)
 
-        var quantity = selectedOrDefault(
-            document.getElementById("quantity").value, 5)
-
-        var subject = selectedOrDefault(
-            document.getElementById("subject").value, 9)
+        var difficulty = getElemValue("difficulty");
+        var quantity = getElemValue("quantity");
+        var subject = allowedSubjs[getElemValue("subject")];
 
         var xhr = new XMLHttpRequest();
         xhr.onload = function () {
@@ -118,15 +124,15 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         var difPath = "https://opentdb.com/api.php?amount=" + quantity + "&category=" + subject + "&difficulty=" + difficulty + "&type=boolean";
+        console.log(difPath);
         xhr.open('GET', difPath, true);
         xhr.responseType = 'text';
         xhr.send();
-
     }
 
     document.getElementById("go").addEventListener("click", () => {
         document.getElementById("settings").style.display = "none";
-        document.getElementById("article").innerHTML = setComplex();
+        setComplex();
     });
 
     document.getElementById("falses").addEventListener("click", () => {
---
+++ objects.js
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <script>
+        const allowedSubjs = { 'art' :25, 'books':10, 'computers' :18 };
+console.log(allowedSubjs);
+        </script>
+    </body>
+</html>
+    
